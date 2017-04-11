4531c36266f87ae76072210b6f244856a12ec552
Elliot Zoerner <Elliot@Elliots-MacBook-Pro-3.local>
2017-04-12 09:06:04 +0930

    Set Up structure

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className='my-content'>
+            <h1>Game Of Life</h1>
+            <p>This is a React Template</p>
+         </div>
+      );
+   }
+}
+
+export default App;
